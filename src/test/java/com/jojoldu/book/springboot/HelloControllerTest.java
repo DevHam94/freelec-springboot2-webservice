0ce329702819925e940bed2e4d9181928434cbd1
backwizard0 <gkaeogus7@gmail.com>
2022-12-21 12:09:10 +0900

    update

---
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -0,0 +1,48 @@
+package com.jojoldu.book.springboot;
+
+import com.jojoldu.book.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+//스프링부트테스트와 JUnit의 연결자역할
+@RunWith(SpringRunner.class)
+//컨트롤러를 사용할수있다.
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc; //웹 api를 테스트할때 사용. 스프링 mvc테스트의 시작점
+
+    //
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello")) //get요청
+                .andExpect(status().isOk()) //ok 200인지 검증
+                .andExpect(content().string(hello)); //hello값이 리턴되는지 확인한다.
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+
+
+}
